5dc64542fe5bb3081a85101f8c23fbc35439ad41
Adil Nurkali <a.nurkaliyev@platform.live>
2022-04-22 16:52:21 +0200

    add filter feature

--- pages/search.js
+++ pages/search.js
@@ -3,13 +3,14 @@ import { useRouter } from 'next/router';
 import Image from 'next/image';
 import { Flex, Box, Text, Icon } from '@chakra-ui/react'
 import { BsFilter } from 'react-icons/bs'
+import { baseUrl, fetchApi } from '../utils/fetchApi'
 
 import SearchFilters from '../components/SearchFilters';
 import Property from '../components/Property';
 import noresult  from '../assets/images/noresult.png'
 
-const Search = () => {
-    const [searchFilters, setSearchFilters] = useState(false)
+const Search = ({ properties }) => {
+    const [searchFilters, setSearchFilters] = useState(true)
     const router = useRouter()
 
     return (
@@ -34,9 +35,9 @@ const Search = () => {
                 Properties {router.query.purpose}
             </Text>
             <Flex flexWrap='wrap'>
-                {[].map((property) => <Property property={property} key={property.id} />)}
+                {properties.map((property) => <Property property={property} key={property.id} />)}
             </Flex>
-            {[].length === 0 && (
+            {properties.length === 0 && (
                 <Flex justifyContent='center' alignItems='center' flexDirection='column' marginTop='5' marginBottom='5'>
                     <Image src={noresult} alt='no result' />
                 </Flex>
@@ -46,3 +47,24 @@ const Search = () => {
 }
 
 export default Search;
+
+export async function getServerSideProps({ query }) {
+    const purpose = query.purpose || 'for-rent';
+    const rentFrequency = query.rentFrequency || 'yearly';
+    const minPrice = query.minPrice || '0';
+    const maxPrice = query.maxPrice || '1000000';
+    const roomsMin = query.roomsMin || '0';
+    const bathsMin = query.bathsMin || '0';
+    const sort = query.sort || 'price-desc';
+    const areaMax = query.areaMax || '35000';
+    const locationExternalIDs = query.locationExternalIDs || '5002';
+    const categoryExternalID = query.categoryExternalID || '4';
+  
+    const data = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`);
+  
+    return {
+      props: {
+        properties: data?.hits,
+      },
+    };
+  }

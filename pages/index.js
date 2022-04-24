e3676543f2c9831b4983bead8ea5272fb87f6466
qipchaq <nurkali.adil@gmail.com>
2022-04-24 13:07:25 +0200

    complete project

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,18 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { Flex, Box, Text, Button } from '@chakra-ui/react'
+import { Flex, Box, Text, Button, Divider } from '@chakra-ui/react'
 
 import { baseUrl, fetchApi } from '../utils/fetchApi'
 import Property from '../components/Property';
 
 const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
-  <Flex flexWrap='wrap' justifyContent='center' alignItems='center' m='10'>
-    <Image src={imageUrl} width={500} height={300}alt='banner' />
+  <Flex bg='gray.100' flexWrap='wrap' justifyContent='center' alignItems='center' p='10'>
+    <Image src={imageUrl} width={500} height={300} alt='banner' />
     <Box p="5">
       <Text color='gray.500' fontSize='sm' fontWeight='medium'>{purpose}</Text>
       <Text fontSize='3xl' fontWeight='bold'>{title1}<br />{title2}</Text>
       <Text fontSize='lg' paddingTop='3' paddingBottom='3' color='gray.700' fontWeight='medium'>{desc1}<br />{desc2}</Text>
-      <Button fontSize='xl'>
+      <Button fontSize='xl' border='2px' borderColor='gray.500'>
         <Link href={linkName}><a>{buttonText}</a></Link>
       </Button>
     </Box>
@@ -22,7 +22,7 @@ const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, i
 export default function Home({ propertiesForSale, propertiesForRent }) {
   return (
     <Box>
-      <Banner       
+      <Banner
         purpose='RENT A HOME'
         title1='Rental Homes for'
         title2='Everyone'
@@ -32,10 +32,12 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
         linkName='/search?purpose=for-rent'
         imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
       />
-      <Flex flexWrap='wrap'>
+      <Divider />
+      <Flex flexWrap='wrap' justifyContent='center' marginTop='10' marginBottom='10'>
         {propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
       </Flex>
-      <Banner 
+      <Divider />
+      <Banner
         purpose='BUY A HOME'
         title1=' Find, Buy & Own Your'
         title2='Dream Home'
@@ -45,8 +47,9 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
         linkName='/search?purpose=for-sale'
         imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
       />
-      <Flex flexWrap='wrap'>
-        {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}        
+      <Divider />
+      <Flex flexWrap='wrap' justifyContent='center' marginTop='10'>
+        {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
       </Flex>
     </Box>
   )
--- pages/search.js
+++ pages/search.js
@@ -7,7 +7,7 @@ import { baseUrl, fetchApi } from '../utils/fetchApi'
 
 import SearchFilters from '../components/SearchFilters';
 import Property from '../components/Property';
-import noresult  from '../assets/images/noresult.png'
+import noresult from '../assets/images/noresult.png'
 
 const Search = ({ properties }) => {
     const [searchFilters, setSearchFilters] = useState(true)
@@ -34,12 +34,13 @@ const Search = ({ properties }) => {
             <Text fontSize='2xl' p='4' fontWeight='bold'>
                 Properties {router.query.purpose}
             </Text>
-            <Flex flexWrap='wrap'>
+            <Flex flexWrap='wrap' justifyContent='center'>
                 {properties.map((property) => <Property property={property} key={property.id} />)}
             </Flex>
             {properties.length === 0 && (
                 <Flex justifyContent='center' alignItems='center' flexDirection='column' marginTop='5' marginBottom='5'>
                     <Image src={noresult} alt='no result' />
+                    <Text fontSize='xl' marginTop='3'>No Result Found.</Text>
                 </Flex>
             )}
         </Box>
@@ -59,12 +60,12 @@ export async function getServerSideProps({ query }) {
     const areaMax = query.areaMax || '35000';
     const locationExternalIDs = query.locationExternalIDs || '5002';
     const categoryExternalID = query.categoryExternalID || '4';
-  
+
     const data = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`);
-  
+
     return {
-      props: {
-        properties: data?.hits,
-      },
+        props: {
+            properties: data?.hits,
+        },
     };
-  }
+}
